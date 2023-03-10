8e244c4c355e23fdd915b32b2bd11f294567e703
varsha <vbhatia_2009@ymail.com>
2023-03-10 11:10:20 +0000

    Add comments to understand the API call for Books

--- app/src/main/java/com/varsha/bookstore/network/BookStoreApi.kt
+++ app/src/main/java/com/varsha/bookstore/network/BookStoreApi.kt
@@ -8,9 +8,17 @@ import javax.inject.Singleton
 
 @Singleton
 interface BookStoreApi {
+    /**
+     * Get list of books
+     * Return List of BookResponse
+     */
     @GET("books")
     suspend fun getBooksData(): List<BookResponse>
 
+    /**
+     * Get book details based on bookid passed as parameter
+     * Returns the book detail
+     */
     @GET("book/{id}")
     suspend fun getBookFromBookId(@Path("id") id: Int): BookDetailResponse
 }
