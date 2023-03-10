3efa68bb08ea880681949a97577d891cf50881d9
varsha <vbhatia_2009@ymail.com>
2023-03-10 12:21:22 +0000

    Adjust imports to fix detekt issues

--- app/src/main/java/com/varsha/bookstore/component/booklist/HomeBookListScreen.kt
+++ app/src/main/java/com/varsha/bookstore/component/booklist/HomeBookListScreen.kt
@@ -1,8 +1,18 @@
 package com.varsha.bookstore.component.booklist
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.primarySurface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
